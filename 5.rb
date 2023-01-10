92797b305736c9e064fe076e79463beef8320aca
Dhairya Sonaiya <71845402+dhairya370@users.noreply.github.com>
2023-01-10 12:22:19 +0000

    completed code

---
+++ 5.rb
@@ -0,0 +1,34 @@
+class Prime
+  def initialize 
+     print "Enter the number : "
+     @num = gets.chomp.to_i
+  end
+
+  
+  def check_prime(num)
+    
+    out = true
+    for i in 2..num-1
+        if num % i == 0
+            out = false
+            break
+         end
+     end
+     return out 
+  end
+
+  
+  def prime_series
+    num = @num
+    for i in 2..num 
+        if check_prime(i) == true 
+           print "#{i}, "
+        end 
+    end
+    puts
+  end
+  
+end
+
+obj = Prime.new
+obj.prime_series
