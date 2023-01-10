bb5f4ed9f5f42a6371ad69b9d0f7d1e026dcd074
Dhairya Sonaiya <71845402+dhairya370@users.noreply.github.com>
2023-01-10 10:58:15 +0000

    completed code

--- 3.rb
+++ 3.rb
@@ -1,3 +1,20 @@
-Class Pali
- def 
+class Palindrome
+  
+  def initialize
+    print "enter the string : "
+    @str = gets.chomp.to_s 
+  end  
+
+  def check
+      str = @str
+      if str.reverse.upcase == str.upcase
+        puts "#{str} is palindrome"
+      else
+        puts "Not Palindrome"
+      end
+  end
 end
+
+obj = Palindrome.new
+obj.check
+ 
